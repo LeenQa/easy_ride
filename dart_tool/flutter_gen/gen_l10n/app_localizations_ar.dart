548b6df0e643dab5cc9664bb4df74fc05468aab9
leenqa <201706172@bethlehem.edu>
2021-03-05 17:35:33 +0200

    dart tools

---
+++ dart_tool/flutter_gen/gen_l10n/app_localizations_ar.dart
@@ -0,0 +1,14 @@
+
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: unnecessary_brace_in_string_interps
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get helloWorld => 'هلا';
+}
