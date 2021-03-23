b66936baad6186f1f2d1fb022eb6a1a95c0caf10
Alex <alexdukmak@hotmail.com>
2021-03-23 17:53:39 +0200

    finished profile screen design

--- lib/constants.dart
+++ lib/constants.dart
@@ -8,4 +8,4 @@ const kSecondaryColor = Color(0xFFFFDC59);
 const kPrimaryDarkColor = Color(0xffff914d);
 const kPrimaryLightColor = Color(0xFFffefe6);
 final Color greenColor = Colors.green.shade400;
-const Color blueColor = Colors.lightBlueAccent;
+const Color blueColor = Colors.blueAccent;
